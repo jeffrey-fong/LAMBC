e29b5f47d78f3ad39fac1ce65cb9e65349ac3f0c
Jeffrey <jeffreyfong94@gmail.com>
2020-10-26 11:34:15 +0800

    Rearranged file structure

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ import os
 import argparse
 
 from lamb_optim.lambc import Lambc
+from models import lenet
+from model import Model
 
+
+def train(args, model):
+	print('train')
+
+def test(args, model):
+	print('test')
+
+def main(args):
+	model = Model(args)
+	train(args, model)
+	test(args, model)
 
 
 
@@ -16,10 +29,13 @@ from lamb_optim.lambc import Lambc
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='LAMB with Adaptive Learning Rate Clipping')
 	parser.add_argument('--lr', type=float, default=0.1)
+	parser.add_argument('--epoch', type=int, default=150)
 	parser.add_argument('--n', type=int, default=3)
 	parser.add_argument('--dataset', type=str, default='MNIST')
 	parser.add_argument('--device', type=str, default='cpu')
+	parser.add_argument('--save_dir', type=str, default='./')
 	args = parser.parse_args()
 
 	args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-	print('test')
+
+	main(args)
---
+++ model.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+from models import lenet
+
+class Model(nn.Module):
+	def __init__(self, args):
+		super(Model, self).__init__()
+		# Model
+		if args.dataset == 'MNIST':
+			self.model = lenet.LeNet(input_dim=1)
+		elif args.dataset == 'CIFAR10':
+			# KIV
+			pass
+
+	def save(self, path):
+		checkpoint = {'model': self.state_dict()}
+		torch.save(checkpoint, path)
+
+	def load(self, path):
+		path = os.path.abspath(path)
+		checkpoint = torch.load(path)
+		self.load_state_dict(checkpoint['model'])
