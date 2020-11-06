a4504d2220ccba62af46ac9be6fe29844654e63b
Jeffrey <jeffreyfong94@gmail.com>
2020-11-06 22:21:15 +0800

    Rearranged for test in each epoch

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ from model import Model
 
 avg_accuracy = 0.0
 train_iter, test_iter = 0, 0
+train_correct, train_total = 0, 0
+test_correct, test_total = 0, 0
 writer = SummaryWriter()
 
 def train(model, train_loader):
-	global train_iter, writer
-	print('train')
+	global train_iter, writer, train_correct, train_total
 	model.train()
 	opt = Lambc(model.parameters(), lr=args.lr, weight_decay=args.weight_decay,
 					betas=(.9, .999), adam=False, clip=args.clip, 
@@ -29,30 +30,25 @@ def train(model, train_loader):
 	criterion = nn.CrossEntropyLoss()
 	losses = []
 
-	for epoch in range(args.epochs):
-		epoch_loss = 0
-		total, correct = 0, 0
-		for image, target in train_loader:
-			image, target = image.to(args.device), target.to(args.device)
-			opt.zero_grad()
-			output = model.forward(image)
-			loss = criterion(output, target)
-			writer.add_scalar("Loss/train", loss, train_iter)
-			loss.backward()
-			opt.step()
-			losses.append(loss.item())
-			_, predicted = output.max(1)
-			total += target.size(0)
-			correct += predicted.eq(target).sum().item()
-			writer.add_scalar("Accuracy/train", 100.*correct/total, train_iter)
-			train_iter += 1
+	for image, target in train_loader:
+		image, target = image.to(args.device), target.to(args.device)
+		opt.zero_grad()
+		output = model.forward(image)
+		loss = criterion(output, target)
+		writer.add_scalar("Loss/train", loss, train_iter)
+		loss.backward()
+		opt.step()
+		losses.append(loss.item())
+		_, predicted = output.max(1)
+		train_total += target.size(0)
+		train_correct += predicted.eq(target).sum().item()
+		writer.add_scalar("Accuracy/train", 100.*train_correct/train_total, train_iter)
+		train_iter += 1
 
-		# Print the current status
-		print("-" * 25)
-		print("Epoch:{:10}".format(epoch))
-		print("Train Loss:{:10.6}\t".format(np.mean(losses)))
-		print("Accuracy:{:10.6}\t".format(100.*correct/total))
-		writer.flush()
+	# Print the current status
+	print("Train Loss:{:10.6}\t".format(np.mean(losses)))
+	print("Accuracy:{:10.6}\t".format(100.*train_correct/train_total))
+	writer.flush()
 
 	# Save and update the model after every full training round
 	if not os.path.exists(args.save_dir):
@@ -60,34 +56,32 @@ def train(model, train_loader):
 	model.save(args.save_dir + "model" + ".pt")
 
 def test(model, test_loader):
-	global avg_accuracy, test_iter, writer
+	global avg_accuracy, test_iter, writer, test_correct, test_total
 	if os.path.exists(args.save_dir + 'model.pt'):
 			model.load(args.save_dir + 'model.pt')
-	print('test')
 	model.eval()
 	criterion = nn.CrossEntropyLoss()
 	losses = []
-	total, correct = 0, 0
 	for image, target in test_loader:
 		image, target = image.to(args.device), target.to(args.device)
 		output = model.forward(image)
 		loss = criterion(output, target)
 		losses.append(loss.item())
 		_, predicted = output.max(1)
-		total += target.size(0)
-		correct += predicted.eq(target).sum().item()
-		test_iter += 1
+		test_total += target.size(0)
+		test_correct += predicted.eq(target).sum().item()
 	# Print the current status
-	print("-" * 25)
 	print("Test Loss:{:10.6}\t".format(np.mean(losses)))
-	print("Accuracy:{:10.6}\t".format(100.*correct/total))
-	avg_accuracy += (100.*correct/total)
-	writer.add_scalar("Accuracy/test", 100.*correct/total, test_iter)
+	print("Accuracy:{:10.6}\t".format(100.*test_correct/test_total))
+	avg_accuracy += (100.*test_correct/test_total)
+	writer.add_scalar("Accuracy/test", 100.*test_correct/test_total, test_iter)
+	test_iter += 1
 	writer.flush()
 
 
 def main():
-	global avg_accuracy
+	global avg_accuracy, train_iter, test_iter, \
+			train_correct, train_total, test_correct, test_total
 	# Dataset
 	if args.dataset == 'MNIST':
 		transform = transforms.Compose([transforms.Resize((32, 32)),
@@ -118,8 +112,11 @@ def main():
 
 
 	model = Model(args).to(args.device)
-	train(model, train_loader)
-	test(model, test_loader)
+	for epoch in range(args.epochs):
+		print("Epoch:{:10}".format(epoch))
+		train(model, train_loader)
+		test(model, test_loader)
+		print("-" * 25)
 	'''for i in range(10):
 		print('Epoch:', i)
 		# Network Model
@@ -137,7 +134,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='LAMB with Adaptive Learning Rate Clipping')
 	parser.add_argument('--lr', type=float, default=0.01)
 	parser.add_argument('--weight_decay', type=float, default=0.0)
-	parser.add_argument('--clip', type=bool, default=True)
+	parser.add_argument('--clip', type=bool, default=False)
 	parser.add_argument('--clip_bound', type=float, default=5.0)
 	parser.add_argument('--epochs', type=int, default=80)
 	parser.add_argument('--batch_size', type=int, default=1000)
