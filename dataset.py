bec25ccbf9c374fa8dbc4fd80977603da2c97d11
Unknown <chensiwei0729@gmail.com>
2020-11-23 15:31:41 +0800

    change to relative path

--- dataset.py
+++ dataset.py
@@ -16,7 +16,7 @@ class ImageNetDataset(Dataset):
 
         if self.train:
             for i in range(1):
-                data_path = f"/home/summer/Documents/GitHub/image_datasets/Imagenet64_train_part1_npz/train_data_batch_{i + 1}.npz"
+                data_path = f"../image_datasets/Imagenet64_train_part1_npz/train_data_batch_{i + 1}.npz"
                 data_seg = np.load(data_path)
                 image_seg = np.reshape(data_seg['data'], (-1, 3, 64, 64)).astype(np.float32)
                 label_seg = data_seg['labels']
@@ -25,7 +25,7 @@ class ImageNetDataset(Dataset):
                 label = label_seg if label is None else np.concatenate((label, label_seg), axis=0)
 
         else:
-            data_path = f"/home/summer/Documents/GitHub/image_datasets/Imagenet64_val_npz/val_data.npz"
+            data_path = f"../image_datasets/Imagenet64_val_npz/val_data.npz"
             data_seg = np.load(data_path)
             data = np.reshape(data_seg['data'], (-1, 3, 64, 64)).astype(np.float32)
             label = data_seg['labels']
